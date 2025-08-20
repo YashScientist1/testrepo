59bcf5a9375638e3b647083cb74ebe40dd48d84e
YashScientist1 <yashscientist2006@gmail.com>
2025-08-20 07:45:49 +0530

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -1,3 +1,10 @@
-# diplaying the python outpu
+# diplaying the python output
 
 print("New Python File")
+
+print("Python is a great way to start learning coding")
+
+a= "As python is Simple and most efficient coding languages till date "
+print(a)
+
+
